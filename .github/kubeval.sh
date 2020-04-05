257edbab627e462b86827f01c243d6d3e17f11fb
Jamie Magee <jamie.magee@gmail.com>
2020-04-05 18:17:17 +0200

    fix: use ci values for kubeval

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -11,5 +11,5 @@ tar -xf /tmp/kubeval.tar.gz kubeval
 
 # validate charts
 for CHART_DIR in ${CHART_DIRS}; do
-  helm template "${CHART_DIR}" | ./kubeval --strict --ignore-missing-schemas --kubernetes-version "${KUBERNETES_VERSION#v}" --schema-location "${SCHEMA_LOCATION}"
+  helm template --values "${CHART_DIR}"/ci/ci-values.yaml "${CHART_DIR}" | ./kubeval --strict --ignore-missing-schemas --kubernetes-version "${KUBERNETES_VERSION#v}" --schema-location "${SCHEMA_LOCATION}"
 done
