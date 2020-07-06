d3369b2db18759d5423e4e60c4cc71cff908d66d
Jamie Magee <JamieMagee@users.noreply.github.com>
2020-07-06 10:45:39 +0200

    ci: drop deprecated k8s (#53)

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/master -- charts | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
-KUBEVAL_VERSION="0.14.0"
+KUBEVAL_VERSION="0.15.0"
 SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/"
 
 # install kubeval
