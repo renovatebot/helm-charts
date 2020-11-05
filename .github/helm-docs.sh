62fbdcd0738dbd591803d9cd40a8a558e0d58e17
Michael Kriese <michael.kriese@visualon.de>
2020-11-05 06:51:12 +0100

    chore: enable renovate

--- .github/helm-docs.sh
+++ .github/helm-docs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
+# renovate: datasource=github-releases depName=norwoodj/helm-docs
 HELM_DOCS_VERSION="0.15.0"
 
 # install helm-docs
--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -euxo pipefail
 
-CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/master -- charts | cut -d '/' -f 2 | uniq)"
+
+# renovate: datasource=github-releases depName=instrumenta/kubeval
 KUBEVAL_VERSION="0.15.0"
+
+CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/master -- charts | cut -d '/' -f 2 | uniq)"
 SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/"
 
 # install kubeval
