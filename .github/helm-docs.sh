0ed73b04800fe441a02160ac9601bd446ef2c2b1
Michael Kriese <michael.kriese@visualon.de>
2021-01-11 12:15:33 +0100

    chore: no quotes

--- .github/helm-docs.sh
+++ .github/helm-docs.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 # renovate: datasource=github-releases depName=norwoodj/helm-docs
-HELM_DOCS_VERSION="1.4.0"
+HELM_DOCS_VERSION=1.4.0
 
 # install helm-docs
 curl --silent --show-error --fail --location --output /tmp/helm-docs.tar.gz https://github.com/norwoodj/helm-docs/releases/download/v"${HELM_DOCS_VERSION}"/helm-docs_"${HELM_DOCS_VERSION}"_Linux_x86_64.tar.gz
