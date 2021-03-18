3e0cf63cc9312a7b2ba4da1db852223daa42bcee
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2021-03-18 16:09:19 -0400

    Add redis subchart (#114)
    Co-authored-by: Jamie Magee <JamieMagee@users.noreply.github.com>

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -12,7 +12,11 @@ SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-s
 curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz https://github.com/instrumenta/kubeval/releases/download/"${KUBEVAL_VERSION}"/kubeval-linux-amd64.tar.gz
 tar -xf /tmp/kubeval.tar.gz kubeval
 
+# add helm repos
+helm repo add bitnami https://charts.bitnami.com/bitnami
+
 # validate charts
 for CHART_DIR in ${CHART_DIRS}; do
+  (cd charts/${CHART_DIR}; helm dependency build)
   helm template --values charts/"${CHART_DIR}"/ci/ci-values.yaml charts/"${CHART_DIR}" | ./kubeval --strict --ignore-missing-schemas --kubernetes-version "${KUBERNETES_VERSION#v}" --schema-location "${SCHEMA_LOCATION}"
 done
