996deff8d6e659905d63e4cb0ac5c04b641b6d1e
Michael Kriese <michael.kriese@visualon.de>
2021-05-17 11:02:57 +0200

    refactor: migrate to new buildpack

--- .devcontainer/tools/kind.sh
+++ .devcontainer/tools/kind.sh
@@ -2,9 +2,9 @@
 
 set -euo pipefail
 
-check_semver "$KIND_VERSION"
+check_semver "$TOOL_VERSION"
 
-curl --silent --show-error --fail --location --output /usr/local/bin/kind "https://github.com/kubernetes-sigs/kind/releases/download/v$KIND_VERSION/kind-linux-amd64"
+curl --silent --show-error --fail --location --output /usr/local/bin/kind "https://github.com/kubernetes-sigs/kind/releases/download/v$TOOL_VERSION/kind-linux-amd64"
 chmod +x /usr/local/bin/kind
 
 kind version
