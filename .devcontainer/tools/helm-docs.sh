4bf71127a751a42828a158cba7a8c14aec123db3
Koen van Zuijlen <8818390+kvanzuijlen@users.noreply.github.com>
2023-09-21 17:57:02 +0200

    Update helm-docs.sh

--- .devcontainer/tools/helm-docs.sh
+++ .devcontainer/tools/helm-docs.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 check_semver "$TOOL_VERSION"
 
-curl --silent --show-error --fail --location --output /tmp/helm-docs.tar.gz https://github.com/norwoodj/helm-docs/releases/download/v"$TOOL_VERSION"/helm-docs_"$TOOL_VERSION"_Linux_x86_64.tar.gz
+curl --silent --show-error --fail --location --output /tmp/helm-docs.tar.gz https://github.com/norwoodj/helm-docs/releases/download/v"$TOOL_VERSION"/helm-docs_Linux_x86_64.tar.gz
 tar -xf /tmp/helm-docs.tar.gz -C /usr/local/bin/ helm-docs
 rm /tmp/helm-docs.tar.gz
 
