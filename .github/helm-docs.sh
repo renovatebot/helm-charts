d76396f2c5be5966354eb9ef17e17818f9af208b
Koen van Zuijlen <8818390+kvanzuijlen@users.noreply.github.com>
2023-09-21 17:52:58 +0200

    Update helm-docs.sh

--- .github/helm-docs.sh
+++ .github/helm-docs.sh
@@ -9,7 +9,7 @@ set -euxo pipefail
 HELM_DOCS_VERSION=1.11.2
 
 # install helm-docs
-curl --silent --show-error --fail --location --output /tmp/helm-docs.tar.gz https://github.com/norwoodj/helm-docs/releases/download/v"${HELM_DOCS_VERSION}"/helm-docs_"${HELM_DOCS_VERSION}"_Linux_x86_64.tar.gz
+curl --silent --show-error --fail --location --output /tmp/helm-docs.tar.gz https://github.com/norwoodj/helm-docs/releases/download/v"${HELM_DOCS_VERSION}"/helm-docs_Linux_x86_64.tar.gz
 tar -C .bin/ -xf /tmp/helm-docs.tar.gz helm-docs
 
 # validate docs
