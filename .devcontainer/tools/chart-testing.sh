fc365d0843fe0aad9a5110a4affbf581aff4f63c
Michael Kriese <michael.kriese@visualon.de>
2023-11-30 16:07:55 +0100

    chore: simplify devcontainer (#738)

--- .devcontainer/tools/chart-testing.sh
+++ .devcontainer/tools/chart-testing.sh
@@ -2,7 +2,11 @@
 
 set -euo pipefail
 
+require_root
+
 check_semver "$TOOL_VERSION"
+
+pip install yamale yamllint
 
 curl --silent --show-error --fail --location --output /tmp/chart-testing.tar.gz https://github.com/helm/chart-testing/releases/download/v"$TOOL_VERSION"/chart-testing_"$TOOL_VERSION"_linux_amd64.tar.gz
 tar -xf /tmp/chart-testing.tar.gz -C /usr/local/bin/ ct
