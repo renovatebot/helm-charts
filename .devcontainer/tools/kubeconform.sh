ed9dc724713b414d9ea88a543419183650555a26
Michael Kriese <michael.kriese@visualon.de>
2025-09-08 10:15:34 +0200

    test: use `kubeconform` instead of `kubeval` (#3178)

---
+++ .devcontainer/tools/kubeconform.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+check_semver "$TOOL_VERSION"
+
+curl --silent --show-error --fail --location --output /tmp/kubeconform.tar.gz "https://github.com/yannh/kubeconform/releases/download/v$TOOL_VERSION/kubeconform-linux-amd64.tar.gz"
+tar -xf /tmp/kubeconform.tar.gz -C /usr/local/bin/ kubeconform
+rm /tmp/kubeconform.tar.gz
+
+kubeconform -v
